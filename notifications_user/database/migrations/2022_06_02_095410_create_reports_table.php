28bfb85803b715fda1a1d7913f128d087d1fc3bf
yusuf <yusuffront@gmail.com>
2022-06-02 18:01:23 +0800

    updated several codes --prepared

--- notifications_user/database/migrations/2022_06_02_095410_create_reports_table.php
+++ notifications_user/database/migrations/2022_06_02_095410_create_reports_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reports', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('users_id');
+            $table->text('notes');
+            $table->enum('conditions', ['normal', 'abnormal']);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reports');
     }
 };
